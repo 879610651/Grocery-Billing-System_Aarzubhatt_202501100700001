469be4d582e5236e3c40480f996f97defefa6490
Aarzu Bhatt <aarzubhatt9@gmail.com>
2026-06-13 11:17:46 +0530

    final cost calculation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,7 +3,7 @@ item1 = float(input("Enter price of Item 1: $"))
 item2 = float(input("Enter price of Item 2: $"))
 item3 = float(input("Enter price of Item 3: $"))
 
-# Calculate total cost
+# Calculate  cost
 total = item1 + item2 + item3
 
 discount = 0
